7a0cb0570df687e075f4533dda1cdadd679aed28
reddybhargavi2848 <reddybhargavi2004@gmail.com>
2026-04-11 09:27:53 +0530

    added day2

---
+++ day2/index.js
@@ -0,0 +1,33 @@
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let c = 32;
+// c = 60;
+// console.log(c);
+
+// const b = 54;
+// console.log(b);
+
+// let num1 = 10;
+// let num2 = "10";
+// if(num1 === num2){
+//     console.log(-1);
+// }else {
+//     console.log(1);
+    
+// }
+
+// let x = (2 * 3)
+// console.log(x);
+// let y = (2 ** 3)
+// console.log(y);
+
+// Arrays
+
+let userData = [10, "hello", false, 3.14, null, 'bye']
+
+for (let index = 0; index < userData.length; index++) {
+    console.log(typeof userData[index]);
+    
+}
