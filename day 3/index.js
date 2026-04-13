ac426461adc68ca7ffacd1d3f84ce0c1a4164376
reddybhargavi2848 <reddybhargavi2004@gmail.com>
2026-04-13 09:55:23 +0530

    added day3

--- day 3/index.js
+++ day 3/index.js
@@ -21,19 +21,79 @@
 // console.log(obj);
 
 
-const users = [
-    {
-        id: 1,
-        name: "user1",
-        loc: "hyd"
-    },
-    {
-        id: 2,
-        name: "user2",
-        loc: "hyd"
-    },
-]
-console.log(users[0].name);
+// const users = [
+//     {
+//         id: 1,
+//         name: "user1",
+//         loc: "hyd"
+//     },
+//     {
+//         id: 2,
+//         name: "user2",
+//         loc: "hyd"
+//     },
+// ]
+// console.log(users[0].name);
+
+
 
 // for of - arrays
 // for in - objects
+
+
+
+// function myFun() {
+//     console.log(-1);
+//     return -1
+// }
+
+// console.log(myFun);
+// console.log(myFun());
+
+
+//addition of two numbers
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(add(43, 2));
+
+// function add(num1, num2) {
+//     return num1 - num2;
+// }
+// console.log(add(40, 3));
+
+// //anonymous(it does not contain method name)
+// const myFun = function(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(myFun(30,2));
+// console.log(myFun());
+
+
+const doMath = (num1, num2, op) => {
+    switch (op) {
+        case "+":
+            console.log(`The addition of ${num1} and ${num2} is ${num1 + num2} `);
+            break;
+        case "-":
+            console.log(`The subtraction of ${num1} and ${num2} is ${num1 - num2} `);
+            break;
+        case "*":
+            console.log(`The multiplication of ${num1} and ${num2} is ${num1 * num2} `);
+            break;
+        case "/":
+            console.log(`The division of ${num1} and ${num2} is ${num1 / num2} `);
+            break;
+        case "%":
+            console.log(`The reminder of ${num1} and ${num2} is ${num1 % num2} `);
+            break;
+        case "**":
+            console.log(`The power of ${num1} and ${num2} is ${num1 ** num2} `);
+            break;
+        default:
+            console.log("Please pass a Valid Input");
+            break;
+    }
+}
+
+doMath(2, 3, "*")
