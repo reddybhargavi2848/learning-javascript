64226302a007b526139d6c0a061a9a41f4bdc858
reddybhargavi2848 <reddybhargavi2004@gmail.com>
2026-04-13 09:13:07 +0530

    day3

---
+++ day 3/index.js
@@ -0,0 +1,39 @@
+// const a = [10,20]
+// a[0] = 30;
+// console.log(a);
+
+
+// const obj = {
+//     id : 1,
+//     name: "guest",
+//     myData: [10, "hello", "bye bye"]
+// }
+
+// console.log(obj.loc);
+// console.log(obj);
+// console.log(obj.id);
+// console.log(obj.name);
+// console.log(obj.myData);
+
+// //adding elements in obj
+// obj.loc = "hyd";
+
+// console.log(obj);
+
+
+const users = [
+    {
+        id: 1,
+        name: "user1",
+        loc: "hyd"
+    },
+    {
+        id: 2,
+        name: "user2",
+        loc: "hyd"
+    },
+]
+console.log(users[0].name);
+
+// for of - arrays
+// for in - objects
---
+++ day1/index.js
@@ -0,0 +1,41 @@
+console.log(-1);
+
+var a = 10;
+var b = 20;
+
+if(a > b){
+    console.log(a); 
+}else{
+    console.log(b); 
+}
+
+//greater of two
+var num1;
+var num1;
+var num1 = 10;
+var num2 = null;
+
+console.log(num1);
+console.log(typeof num1);
+
+var num1 = 10;
+var num2 = 2002;
+
+if ( num1 > num2 ) {
+    console.log(num1);
+} else {
+    console.log ( num2 );
+}
+
+// == => check the values
+// ===  - check the datatype and values
+// *  -  multiplication
+// ** - power
+
+var a = 10;
+var b = "10";
+
+if (a == b) console.log(-1); 
+if (a === b) console.log(-1); 
+
+console.log (2 ** 3);
