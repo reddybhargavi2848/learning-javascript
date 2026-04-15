007ac673e9a21b89d615feef93ddbe147ec9f22a
reddybhargavi2848 <reddybhargavi2004@gmail.com>
2026-04-15 10:18:12 +0530

    added day4

---
+++ day 4/index.js
@@ -0,0 +1,19 @@
+//DOM in js(document object model)
+
+//console.log(document);
+//console.log(document.body);
+
+//let buchi = document.getElementById("buchi");
+// console.log(buchi);
+
+//buchi.innerText="<b>hello</b>"
+
+// buchi.innerHTML="<b>hello</b>"
+// let boxes=document.getElementsByClassName("box");
+//console.log(boxes);
+
+//let para=document.getElementsByClassName("para")
+// console.log(para);
+
+//boxes[1].innerHTML = "bye"
+// para[0].innerHTML = "holaa"
