109828c49de682e7664a7972f8a9e9be33518f71
reddybhargavi2848 <reddybhargavi2004@gmail.com>
2026-04-16 09:54:07 +0530

    added day6

---
+++ day 6/index.js
@@ -0,0 +1,40 @@
+
+const add = () => {
+    event.preventDefault()
+
+    let val1 = Number(document.getElementById("value1").value)
+    let val2 = Number(document.getElementById("value2").value)
+    let display = document.getElementById("display")
+
+    display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 + val2}`
+}
+
+const sub = () => {
+    event.preventDefault()
+
+    let val1 = document.getElementById("value1").value
+    let val2 = document.getElementById("value2")
+    let display = document.getElementById("display")
+
+    display.innerHTML = `The sub of ${val1} and ${val2.value} is ${val1 - val2.value}`
+}
+
+const mult = () => {
+    event.preventDefault()
+
+    let val1 = document.getElementById("value1").value
+    let val2 = document.getElementById("value2")
+    let display = document.getElementById("display")
+
+    display.innerHTML = `The mul of ${val1} and ${val2.value} is ${val1 * val2.value}`
+}
+
+const div = () => {
+    event.preventDefault()
+
+    let val1 = document.getElementById("value1").value
+    let val2 = document.getElementById("value2")
+    let display = document.getElementById("display")
+
+    display.innerHTML = `The div of ${val1} and ${val2.value} is ${val1 / val2.value}`
+}
